6778f98c045702b62f2703d4886a2b61c8f0153a
Amar <amar@uub.localdomain>
2022-07-21 11:48:49 +0200

    display-data-acording-to-date

--- frontend/src/pages/Home/style.jsx
+++ frontend/src/pages/Home/style.jsx
@@ -4,8 +4,48 @@ export default styled.main`
   background-color: var(--color-bg-primary);
   height: 80vh;
   overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: var(--font-global);
+
+  .calendar {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    justify-content: space-between;
+    width: 70vw;
+    border: solid 2px #bebebe;
+    border-radius: 30px;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .monthAndYear {
+    color: white;
+  }
+
+  .nextIcon {
+    height: 3rem;
+  }
+
+  .previousIcon {
+    height: 3rem;
+  }
+
+  button {
+    color: var(--color-font-primary);
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
 
   @media screen and (min-width: 700px) {
     height: 81vh;
+
+    .calendar {
+      width: 20vw;
+    }
   }
 `;
